127a8a808bfe638125acde16314beddf33c6dad6
kacharles <charleston.ak@gmail.com>
2024-02-16 03:48:43 -0800

    W05 Task Complete

---
+++ w05-task/practice/scripts/course.js
@@ -0,0 +1,63 @@
+const aCourse = { 
+    code: "CSE121b", 
+    name: "JavaScript Language", 
+    sections: [
+        {
+            sectionNum: 1,
+            roonNum: "STC 353", 
+            enrolled: 26,
+            days: "TTh",
+            instructor: "Bro T",
+
+        }, 
+        {
+            sectionNum: 2,
+            roonNum: "STC 347", 
+            enrolled: 25,
+            days: "TTh",
+            instructor: "Sis A",
+        }, 
+        {
+
+        }
+    ],
+
+    init() { 
+        setCourseInfo(this); 
+        renderSections(this.sections);
+    },
+    
+    changeEnrollment: function (sectionNum, add = true) { 
+        //find the right section using the Array.findIndex method
+        const sectionIndex = this.sections.findIndex((sectio) => sectionIndex.sectionNum == sectionNum);
+        if (sectionIndex >= 0) { 
+            if(add) { 
+                this.sections[sectionIndex].enrolled++;
+            } else {
+                this.sections[sectionIndex].enrolled--;
+            }
+            renderSections(this.sections); 
+        }
+    },
+};
+
+function setCourseInfo(course) { 
+    const courseName = document.querySelector("#courseName");
+    const coursecode = document.querySelector("#courseCode"); 
+    courseName.textContent = course.name;
+    coursecode.textContent = course.code; 
+} 
+
+function renderSections(sections) { 
+    const html = sections.map(
+        (section) => <tr>
+            <td>${section.sectionNum}</td>
+            <td>${section.roomNum}</td>
+            <td>${section.enrolled}</td>
+            <td>${section.days}</td>
+            <td>${section.instructor}</td>
+        </tr>
+    );
+    document.querySelector("#sections").innerHTML = html.join("");
+}
+export default aCourse; 
---
+++ w05-task/scripts/w05-task.js
@@ -0,0 +1,83 @@
+/* W05: Programming Tasks */
+/* Declare and initialize global variables */
+const templesElement = document.querySelector('#temples');
+let templeList = [];
+
+/* async displayTemples Function */
+const displayTemples = (temples) => {
+    // console.log("Displaying temples:", temples);
+    temples.forEach(temple => {
+        // console.log("Displaying Temple", temple);
+        // Create <article> element
+        const articleElement = document.createElement('article');
+
+        // Create <h3> element
+        const headingElement3 = document.createElement('h3');
+        // Add temple's templeName property to this new element
+        headingElement3.textContent = temple.templeName;
+
+        // Create <img> element
+        const imgElement = document.createElement('img');
+        // Add temple's location property to the src attribute
+        imgElement.setAttribute('src', temple.imageUrl);
+        // Add temple's location property to the alt attribute
+        imgElement.setAttribute('alt', temple.location);
+
+        // Append the <h3> element and the <img> element to the <article> as children
+        articleElement.appendChild(headingElement3);
+        articleElement.appendChild(imgElement);
+
+        // Append the <article> element to the global templesElement varible
+        templesElement.appendChild(articleElement);
+    });
+};
+
+/* async getTemples Function using fetch()*/
+const getTemples = async () => {
+    const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
+    const data = await response.json();
+
+    templeList = data;
+
+    displayTemples(templeList);
+};
+
+/* reset Function */
+function reset() {
+    templesElement.innerHTML = "";
+};
+
+/* filterTemples Function */
+const filterTemples = (temples) => {
+    reset();
+    let filter = document.getElementById('filtered').value;
+    // console.log("Filter selected:", filter);
+
+    // Switch statement
+    switch (filter) {
+        case 'utah':
+            let utahTemples = temples.filter(temple => temple.location.includes('Utah'));
+            // console.log("Utah Temples:", utahTemples);
+            displayTemples(utahTemples);
+            break;
+        case 'notutah':
+            let notutahTemples = temples.filter(temple => !temple.location.includes('Utah'));
+            // console.log("Not Utah Temples", notutahTemples);
+            displayTemples(notutahTemples);
+            break;
+        case 'older':
+            let olderTemples = temples.filter(temple => new Date(temple.dedicated) < new Date(1950, 0, 1));
+            // console.log("Older Temples", olderTemples);
+            displayTemples(olderTemples);
+            break;
+        case 'all':
+            displayTemples(temples);
+            break;
+    }
+};
+
+//Call the getTemples() function
+getTemples();
+
+/* Event Listener */
+document.querySelector('#filtered').addEventListener('change', () => { filterTemples(templeList) });
