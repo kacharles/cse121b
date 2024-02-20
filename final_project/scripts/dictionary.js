1d5b2840afd3710c87b2c24180cb246a7acd7f52
kacharles <charleston.ak@gmail.com>
2024-02-20 04:16:50 -0800

    Dictionary App

---
+++ final_project/scripts/dictionary.js
@@ -0,0 +1,41 @@
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result = document.querySelector("#result"); 
+const sound = document.querySelector("#sound");
+const btn = document.querySelector("#search-btn");
+
+btn.addEventListener("click", () => {
+    let inpWord = document.querySelector("#inp-word").value;
+    fetch(`${url}${inpWord}`).then((response) => response.json()).then((data) => {
+        console.log(data)
+        result.innerHTML = `
+        <div class="word">
+        <h3>${inpWord}</h3>
+        <button onclick="playSound();">
+            <i class="fa-sharp fa-solid fa-volume-high"></i>
+        </button>
+    </div>
+    <div class="details">
+        <p>
+            ${data[0].meanings[0].partOfSpeech}
+        </p>
+        <p>
+            /${data[0].phonetic}/
+        </p>
+        
+    </div>
+    <p class="word-meaning">
+        ${data[0].meanings[0].definitions[0].definition}
+    </p>
+    <p class="word-example">
+        ${data[0].meanings[0].definitions[0].example || ""}
+    </p>`;
+    sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
+
+        
+    });
+});
+
+function playSound() { 
+    sound.play();
+    console.log('sound is playing');
+}
